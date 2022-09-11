d13c27cac0a58e52508f86e142235bfe62da13e8
Sasalmarcon <sasalmarcon@gmail.com>
2022-09-12 01:33:40 +0545

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        navLeftLink:'#5A5B6A'
+      }
+    },
   },
   plugins: [],
 }
