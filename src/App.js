9daae9caf7c918fafd5bba45330be55ee43a1960
Sasalmarcon <sasalmarcon@gmail.com>
2022-09-13 12:19:42 +0545

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Appbar from './components/Appbar';
+import Departments from './components/Departments';
 import Navbar from "./components/Navbar";
 import Product from './components/Product';
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar/>
       <Appbar/>
+      <Departments/>
       <Product />
     </div>
   );
